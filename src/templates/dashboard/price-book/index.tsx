a4bcd60b05d17c61f9b433e7a4528e1425e956d3
arc9693 <archana.choudhary.9693@gmail.com>
2021-03-04 04:40:27 +0530

    Add component - discounts for price group

--- src/templates/dashboard/price-book/index.tsx
+++ src/templates/dashboard/price-book/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router";
 import SellingPrices from "./selling-prices";
+import Discounts from "./discounts"
 
 const Routes: React.FC<{ match: any }> = ({ match }) => (
     <>
         <Switch>
           <Route path={`${match.url}/selling-prices`} component={SellingPrices} />
+          <Route path={`${match.url}/discounts`} component={Discounts} />
         </Switch>
     </>
 );
